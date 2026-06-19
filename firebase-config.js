8dc3598365d4e31ceed023077df8d38fe190871a
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-19 11:16:32 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.firebaseConfig = {
-  apiKey: "AIzaSyMaUsiLQ-6fqJIFn3jLb_xTEXOp19Czlo",
+  apiKey: "AIzaSy4aAuSj_0-6fqJTFp3iLh_xTEXOp19CzLo",
   authDomain: "equipamentos-sesi.firebaseapp.com",
   projectId: "equipamentos-sesi",
   storageBucket: "equipamentos-sesi.firebasestorage.app",
   messagingSenderId: "1058448519996",
-  appId: "1:1058448519996:web:21d038c08a1a593c386439"
+  appId: "1:1058448519996:web:21d0388c081a593c386439"
 };

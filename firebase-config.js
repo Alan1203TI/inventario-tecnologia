765bb023013588b551a8d1edff485b755101f171
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-19 10:52:36 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA4aUSj_O-6fqJTFp3iLh_xTEXOpl9CzLo",
-  authDomain: "equipamentos-sesi.firebaseapp.com",
-  projectId: "equipamentos-sesi",
-  storageBucket: "equipamentos-sesi.firebasestorage.app",
-  messagingSenderId: "1058448519996",
-  appId: "1:1058448519996:web:9a969b1b67eddfc9386439"
-};
